a7c489ca6b37c487e1527e4af8ed90bb875647ab
Taslim Yusuf <Omotoyosi2210@gmail.com>
2025-12-11 13:43:58 +0100

    Create task component ui done

--- src/components/creatTask.tsx
+++ src/components/creatTask.tsx
@@ -2,9 +2,17 @@ import { useState } from "react";
 import DatePicker from "./datePicker";
 import TimePicker from "./timePicker";
 import SelectPlatform from "./selectPlatform";
+import { Switch } from "./ui/switch";
+import { Checkbox } from "./ui/checkbox";
 
 function CreateTask() {
     const [selectedDate, setSelectedDate] = useState<Date | undefined>(new Date())
+    const [finalDate, setFinalDate] = useState<Date | undefined>(new Date())
+    const [repeat, setRepeat] = useState(false)
+    const [daily, setDaily] = useState(false)
+    const [weekly, setWeekly] = useState(false)
+    const [monthly, setMonthly] = useState(false)
+    const [minutes, setMinutes] = useState(false)
 
     return ( 
         <div className="py-6 px-4">
@@ -45,6 +53,72 @@ function CreateTask() {
                     <div className="flex w-full gap-6 border-gray-300 border rounded-md p-2">
                         <input type="text" className="p-2 w-full rounded-md border border-gray-200"/>
                         <button className="bg-green-400 rounded-md text-white py-2 px-4 font-medium">Add</button>
+                    </div>
+                </div>
+                <div className="space-y-2 flex gap-5">
+                    <div >
+                        <div className="flex items-center gap-3">
+                            <label className="font-medium">Repeat</label>
+                            <Switch
+                            checked={repeat}
+                            onCheckedChange={setRepeat}
+                            className="
+                            data-[state=checked]:bg-green-500
+                            "
+                            >
+                            </Switch>
+                        </div>
+                        <div className="text-left space-y-2">
+                            <div className="space-x-2">
+                                <Checkbox
+                                disabled={!repeat}
+                                checked={daily}
+                                onCheckedChange={() => setDaily(!daily)}
+                                className="data-[state=checked]:bg-green-500 data-[state=checked]:border-none"
+                                ></Checkbox>
+                                Daily
+                            </div>
+                            <div className="space-x-2">
+                                <Checkbox
+                                disabled={!repeat}
+                                checked={weekly}
+                                onCheckedChange={() => setWeekly(!weekly)}
+                                className="data-[state=checked]:bg-green-500 data-[state=checked]:border-none"
+                                ></Checkbox>
+                                Weekly
+                            </div>
+                            <div className="space-x-2">
+                                <Checkbox
+                                disabled={!repeat}
+                                checked={monthly}
+                                onCheckedChange={() => setMonthly(!monthly)}
+                                className="data-[state=checked]:bg-green-500 data-[state=checked]:border-none"
+                                ></Checkbox>
+                                Monthly
+                            </div>
+                            <div className="space-x-2 flex items-center">
+                                <Checkbox
+                                disabled={!repeat}
+                                checked={minutes}
+                                onCheckedChange={() => setMinutes(!minutes)}
+                                className="data-[state=checked]:bg-green-500 data-[state=checked]:border-none"
+                                ></Checkbox>
+                                <span className="flex items-center gap-2">
+                                    Every
+                                    <input type="number" className="p-1 border rounded-md w-20" />
+                                    minutes
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="border border-l-2 border-l-gray-200 border-white p-2">
+                        <label className="font-medium">Select Final Day <span className="font-light text-gray-300">(Optional)</span></label>
+                        <div className="border rounded-md p-2">
+                            <DatePicker
+                            onDateChange={setFinalDate}
+                            className=""
+                            ></DatePicker>
+                        </div>
                     </div>
                 </div>
                 <div className="border border-white pt-4 border-t-gray-200 flex justify-between">
